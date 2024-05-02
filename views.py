640c607b06659751db5f55022cd62a02f201a737
AN24b <147623121+AN24b@users.noreply.github.com>
2024-05-02 14:45:28 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,77 +1,23 @@
-from cmath import log
-from tkinter import E
-from django.shortcuts import redirect, render
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate , login , logout
-from django.http import HttpResponseRedirect,HttpResponse
-# Create your views here.
-from .models import Profile
-
-
-def login_page(request):
-    
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user_obj = User.objects.filter(username = email)
-
-        if not user_obj.exists():
-            messages.warning(request, 'Account not found.')
-            return HttpResponseRedirect(request.path_info)
-
-
-        if not user_obj[0].profile.is_email_verified:
-            messages.warning(request, 'Your account is not verified.')
-            return HttpResponseRedirect(request.path_info)
-
-        user_obj = authenticate(username = email , password= password)
-        if user_obj:
-            login(request , user_obj)
-            return redirect('/')
-
-        
-
-        messages.warning(request, 'Invalid credentials')
-        return HttpResponseRedirect(request.path_info)
-
-
-    return render(request ,'accounts/login.html')
-
-
-def register_page(request):
-
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user_obj = User.objects.filter(username = email)
-
-        if user_obj.exists():
-            messages.warning(request, 'Email is already taken.')
-            return HttpResponseRedirect(request.path_info)
-
-        print(email)
-
-        user_obj = User.objects.create(first_name = first_name , last_name= last_name , email = email , username = email)
-        user_obj.set_password(password)
-        user_obj.save()
-
-        messages.success(request, 'An email has been sent on your mail.')
-        return HttpResponseRedirect(request.path_info)
-
-
-    return render(request ,'accounts/register.html')
+from django.shortcuts import render
+from products.models import Product
 
 
 
 
-def activate_email(request , email_token):
+def get_product(request , slug):
     try:
-        user = Profile.objects.get(email_token= email_token)
-        user.is_email_verified = True
-        user.save()
-        return redirect('/')
+        product = Product.objects.get(slug =slug)
+        context = {'product':product}
+
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            price = product.get_product_price_by_size(size)
+            context['selected_size'] = size
+            context['updated_price'] = price
+            print(price)
+
+
+        return render(request  , 'product/product.html' , context = context)
+
     except Exception as e:
-        return HttpResponse('Invalid Email token')
+        print(e)
